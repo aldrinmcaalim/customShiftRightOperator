0c16aa4bd8aad208cc2cc813c0f1c210b5ded2df
unknown <aldrinmcaalim@gmail.com>
2021-11-04 10:45:51 -0400

    Added tests

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ function customShiftRight(first, second) {
     return Math.floor(first/(2 ** second));
 };
 
+console.log(customShiftRight(80,3));
+console.log(customShiftRight(-24,2));
+console.log(customShiftRight(-5,1));
+console.log(customShiftRight(4666,6));
+console.log(customShiftRight(3777,6));
+console.log(customShiftRight(-512,10));
